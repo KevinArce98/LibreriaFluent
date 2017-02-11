b97389ac0474b2bc551e104c195fdb297bda5c3c
Kevin Arias <Kevin Arias>
2017-02-11 12:20:32 -0600

    Primer Commit

---
+++ EjemploLibreriaFluent/Logica/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploLibreriaFluent.Logica
+{
+    class Libro
+    {
+        private int libro;
+        private string nombre;
+
+
+    }
+}
